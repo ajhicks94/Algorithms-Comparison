64ae997ebe2d179e4ea03840264d8947e4b42f23
Anthony Hicks <ajhicks94@gmail.com>
2017-11-11 19:44:15 -0600

    Loads matrix with input data

--- comparator.cpp
+++ comparator.cpp
@@ -6,13 +6,19 @@
  */
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 using std::cout;
 using std::endl;
+using std::istringstream;
 using std::vector;
+using std::string;
 using std::ifstream;
 using std::ofstream;
+using std::getline;
+
+
 
 int main(int argc, char* argv[]){
 
@@ -21,11 +27,50 @@ int main(int argc, char* argv[]){
         return 0;
     }
 
+    // Open the input file
     ifstream input;
     input.open(argv[1]);
 
     if(input.is_open()){
-        
+        int n;
+        char fchar;
+        string firstLine;
+
+        // Get the first line
+        getline(input, firstLine);
+
+        // Confirm that we have the line above the matrix
+        if(firstLine[0] == 'n'){
+            cout << "Correctly identified first line" << endl;
+
+            // Parse number of nodes from the first line
+            istringstream iss (firstLine);
+            iss >> fchar >> fchar >> n;
+            
+            // Create vector of vectors with size n x n
+            vector<vector<int> > m (n, vector<int>(n));
+
+            /*input >> m[0][0];
+            cout << "m[0][0]= " << m[0][0] << '\n';
+            input >> m[0][1];
+            cout << "m[0][1]= " << m[0][1] << '\n';
+*/
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    input >> m[i][j];
+                }
+            }
+
+            // Run dijsktra's n times
+            // Run Floyd-Warshall's
+
+            /*for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    cout << m[i][j] << " ";
+                }
+                cout << '\n';
+            }*/
+        }
     }
     else cout << "There was a problem opening the input file: " << argv[1] << endl;
 
